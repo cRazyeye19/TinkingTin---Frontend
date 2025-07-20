c543c68fc7f0a6cea04d5ecd50589eac50315d3d
cRazyeye19 <jlesterpansoy@gmail.com>
2025-07-20 08:59:36 +0800

    chore(config): configure API base URL with environment variable

--- src/api/UploadRequest.js
+++ src/api/UploadRequest.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000' });
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem("profile")) {
